9c686fc3a0477b649a260a6668532bc6db5d1d4b
ferreol <ferreol.soulez@epfl.ch>
2017-04-25 19:08:58 +0200

    add titles in DEMIChooks

--- src/microTiPi/microUtils/DEMICSHook.java
+++ src/microTiPi/microUtils/DEMICSHook.java
@@ -17,22 +17,28 @@ public class DEMICSHook implements TiPiHook{
     private Imager curImager;
     private Shape outShape;
     private boolean debug;
+    private String title=null;
 
     /**
      *
      */
-    public DEMICSHook(Imager imager, Shape outShape, boolean debug) {
+    public DEMICSHook(Imager imager, Shape outShape, String title, boolean debug) {
         this.curImager = imager;
         this.outShape = outShape;
         this.debug = debug;
+        this.title = title;
     }
     /* (non-Javadoc)
      * @see mitiv.utils.TiPiHook#run(java.lang.Object, int)
      */
     @Override
     public void run(Object caller, int iter) {
-        // TODO Auto-generated method stub
-        curImager.show(ArrayUtils.crop(((MicroDeconvolution) caller).solver.getSolution(),outShape) ,"Current mu="+((MicroDeconvolution) caller).solver.getRegularizationLevel() +"it:"+((MicroDeconvolution) caller).solver.getIterations());
+        String titlet;
+        if(title==null)
+            titlet = "Current mu="+((MicroDeconvolution) caller).solver.getRegularizationLevel() +"it:"+((MicroDeconvolution) caller).solver.getIterations();
+        else
+            titlet = title;
+        curImager.show(ArrayUtils.crop(((MicroDeconvolution) caller).solver.getSolution(),outShape),titlet);
 
         if (debug){
             System.out.println("Cost "+((MicroDeconvolution) caller).solver.getCost() );
